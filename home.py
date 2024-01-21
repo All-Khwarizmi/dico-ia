2c3d1ee82dea282a30ad347dcf66d060240d94b5
Jason Suarez <swarecito@gmail.com>
2024-01-21 20:33:58 +0100

    Add sidebar and Q&A

--- home.py
+++ home.py
@@ -5,7 +5,9 @@ from openai import OpenAI
 OPENROUTER_API_KEY = st.secrets["OPENROUTER_API_KEY"]
 
 
-st.title("Dico")
+st.title("DicoIA")
+st.subheader("Un assistant qui aide des élèves de collège à traduire des mots de vocabulaire et à donner des définitions de mots.")
+
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -55,6 +57,24 @@ if prompt:
             }
         )
         
+
+
+     
+        
+# Add sidebar Q&A to tell users in french how to use the app and why thre're certain constraints
+st.sidebar.title("Q&A")
+st.sidebar.markdown("### Comment utiliser DicoIA?")
+st.sidebar.markdown("DicoIA est un assistant qui aide des élèves de collège à traduire des mots de vocabulaire et à donner des définitions de mots. Mais il y a certaines contraintes. DicoIA ne peut traduire que deux mots à la fois MAXIMUM. ")
+st.sidebar.markdown("### Pourquoi DicoIA ne peut traduire que deux mots à la fois MAXIMUM?")
+st.sidebar.markdown("Parce que DicoIA est un assistant qui aide des élèves de collège à traduire des mots, à donner des définitions de mots et à apprendre des mots de vocabulaire. Mais plus important encore, il est conçu pour éduquer les élèves à utiliser un traducteur judicieusement et développer leur autonomie.")
+st.sidebar.markdown("### Comment formuler une question à DicoIA?")
+st.sidebar.markdown("Pour formuler une question à DicoIA, il faut dire: 'traduis-moi le mot 'hola' en français' ou 'traduis-moi le mot 'bonjour' en espagnol' ou 'donne-moi la définition du mot 'hola'' ou 'donne-moi la définition du mot 'bonjour'.")
+st.sidebar.markdown("### Pourquoi les guillemets (\"\") sont-ils nécessaires?")
+st.sidebar.markdown("Les guillemets (\"\") sont nécessaires pour aider DicoIA à déterminer quels mots traduire.")
+
+# Questions et conseils méthodologiques pour développer l'autonomie des élèves et les aider à utiliser un traducteur judicieusement
+st.sidebar.markdown("### Comment je fais si j'ai besoin de traduire une phrase?")
+st.sidebar.markdown("Pour construire une phrase tu dois d'abord réfléchir à ce que tu veux dire. Ensuite, tu dois essayer de trouver les mots qui te permettent de dire ce que tu veux dire. Tu peux te servir du matériel de cours à ta disposition. Fais des phrases simples. ")
 
 
 
